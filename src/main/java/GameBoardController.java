72429b0ff10043c5f9afba2d34ad54e25d75a242
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2021-05-28 16:25:12 +0200

    basic mechanism

--- src/main/java/GameBoardController.java
+++ src/main/java/GameBoardController.java
@@ -27,26 +27,33 @@ public class GameBoardController implements Initializable {
     @FXML
     private RadioButton radioButtonPLAYER;
 
+    @FXML
+    private Label endingText;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
         //wyzerowanie tablicy
         for (Integer[] xPo : xPos) {
             Arrays.fill(xPo, 0);
         }
+        endingText.setVisible(false);
 
         ticTacToeBoard.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
-            public void handle(MouseEvent event) {
-                Label x = new Label("X");
+            public void handle(MouseEvent event) {//1 gracz | -1 AI
 
-                if (xPos[(int) Math.ceil(event.getSceneX() / 200) - 1][(int) Math.ceil(event.getSceneY() / 200) - 1] != 1) {
+                Label x = new Label("X");
+                if (xPos[(int) Math.ceil(event.getSceneX() / 200) - 1][(int) Math.ceil(event.getSceneY() / 200) - 1] != 1 && xPos[(int) Math.ceil(event.getSceneX() / 200) - 1][(int) Math.ceil(event.getSceneY() / 200) - 1] != -1 && !endingText.isVisible()) {
                     ticTacToeBoard.add(x, (int) Math.ceil(event.getSceneX() / 200) - 1, (int) Math.ceil(event.getSceneY() / 200) - 1);
                     xPos[(int) Math.ceil(event.getSceneX() / 200) - 1][(int) Math.ceil(event.getSceneY() / 200) - 1] = 1;
-                    pcMove();
+                    winningLosingCheck(xPos, false);
+                    if (!endingText.isVisible()) {
+                        pcMove();
+                    }
                 }
             }
         });
+
     }
 
     public void pcMove(){
@@ -55,26 +62,58 @@ public class GameBoardController implements Initializable {
         outer:
         for (int x = 0; x < 3; x++){
             for (int y = 0; y < 3; y++){
-                if (xPos[x][y] != 1){
+                if (xPos[x][y] != 1 && xPos[x][y] != -1){
                     ticTacToeBoard.add(X, x, y);
-                    xPos[x][y] = 1;
+                    xPos[x][y] = -1;
                     break outer;
                 }
             }
         }
-
+        winningLosingCheck(xPos,true);
         //wyswietlanie
         for (int a = 0; a < xPos.length; a++){
             for (int b = 0; b < xPos[a].length; b++ ){
-                System.out.print(xPos[b][a] + ", ");
+                System.out.print(xPos[a][b] + ", ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
-
-    public void miniMax(){
-
+    public void winningLosingCheck(Integer[][] posToCheck, boolean AI){
+        //poziomo
+        if (posToCheck[0][0] != 0 && posToCheck[0][1] != 0 && posToCheck[0][2] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
+        if (posToCheck[1][0] != 0 && posToCheck[1][1] != 0 && posToCheck[1][2] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
+        if (posToCheck[2][0] != 0 && posToCheck[2][1] != 0 && posToCheck[2][2] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
+        //pionowo
+        if (posToCheck[0][0] != 0 && posToCheck[1][0] != 0 && posToCheck[2][0] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
+        if (posToCheck[0][1] != 0 && posToCheck[1][1] != 0 && posToCheck[2][1] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
+        if (posToCheck[0][2] != 0 && posToCheck[1][2] != 0 && posToCheck[2][2] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
+        //przekatne
+        if (posToCheck[0][0] != 0 && posToCheck[1][1] != 0 && posToCheck[2][2] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
+        if (posToCheck[0][2] != 0 && posToCheck[1][1] != 0 && posToCheck[2][0] != 0 ) if (AI) {endingText.setText("You Won!"); endingText.setVisible(true);} else {endingText.setText("You Lost!"); endingText.setVisible(true);}
     }
+
+    /*public int miniMax(int difficultyLvL,boolean AI, Integer[][] posTable){
+
+        if (AI) {
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    if (posTable[x][y] == 0){
+                        posTable[x][y] = -1;
+                        return miniMax(difficultyLvL,false,posTable);
+                    }
+                }
+            }
+        }else {
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    if (posTable[x][y] == 0){
+                        posTable[x][y] = 1;
+                        return miniMax(difficultyLvL,true,posTable);
+                    }
+                }
+            }
+        }
+    }*/
 }
